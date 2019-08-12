51011144d6e4148b4b495d76d3ec0be168ced140
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-08-11 21:20:34 -0700

    Added save button

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,11 @@ const App = () => {
       <div>
         <SavedList list={savedList} />
         <Route path="/" exact component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route path="/movies/:id"
+          render={props => {
+            return <Movie {...props} addToSavedList={addToSavedList} />
+          }}
+        />
       </div>
     </Router>
   );
